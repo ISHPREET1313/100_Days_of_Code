def01179bfa735c40ea011885c61bb5becfb3144
ISHPREET1313 <ishpreetsingh13ips@gmail.com>
2025-10-16 00:00:10 +0530

    Day 53

---
+++ Q__103.c
@@ -0,0 +1,54 @@
+// Q103: Write a Program to take an array of integers as input, calculate the pivot index of this array. The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right. If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array. Print the leftmost pivot index. If no such index exists, print -1.
+
+/*
+Sample Test Cases:
+Input 1:
+nums = [1,7,3,6,5,6]
+Output 1:
+3
+
+Input 2:
+nums = [1,2,3]
+Output 2:
+-1
+
+Input 3:
+nums = [2,1,-1]
+Output 3:
+0
+
+*/
+
+#include<stdio.h>
+
+int main(){
+    int count;
+    printf("Enter how many numbers:");
+    scanf("%d",&count);
+    int nums[count];
+    for(int i=0;i<count;i++){
+        scanf("%d",&nums[i]);
+    }
+    int left_sum,right_sum,i,var=1;
+    for(i=0;i<count;i++){
+        left_sum=0;
+        right_sum=0;
+        for(int j=0;j<i;j++){
+            left_sum+=nums[j];
+        }
+        for(int k=i+1;k<count;k++){
+            right_sum+=nums[k];
+        }
+        if(left_sum==right_sum){
+            var=0;
+            break;
+        }
+    }
+    if(var){
+        printf("-1");
+    }
+    else{
+        printf("%d",i);
+    }
+    return 0;
+}
